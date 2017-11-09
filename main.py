3aa08c714f910c69c542e8a64cdd2318ebe94ba5
Christian Andersen <c.andersen2012@gmail.com>
2017-11-08 22:01:49 -0600

    Fixing some stuff up :joy:

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
+import os
+import tkinter as tk
+
 import spotipy.util as util
 
 import export as Export
+import spotify_import as Import
 
-client_id = "e6376f7ecbfb45799240881e368d4d18"
-client_secret = "47d3d26c9ba9492a9b0e6cc278712c99"
-redirect_uri = "https://anderc.com"
-username = 'c.andersen2012@gmail.com'
-scopes = 'user-library-read user-follow-read'
 
-token = util.prompt_for_user_token(username, scopes, client_id=client_id,
-                                   client_secret=client_secret, redirect_uri=redirect_uri)
+# Functions
 
-Export.export_playlists(token)
-Export.export_follows(token)
-Export.export_library_tracks(token)
+
+def backup():
+    from_token = util.prompt_for_user_token(username_from, scopes, client_id=client_id_from,
+                                            client_secret=client_secret_from, redirect_uri=redirect_uri)
+    Export.export_playlists(from_token)
+    Export.export_follows(from_token)
+    Export.export_library_tracks(from_token)
+    os.remove('.cache-' + username_from)
+
+
+def restore():
+    to_token = util.prompt_for_user_token(username_to, scopes, client_id=client_id_to, client_secret=client_secret_to,
+                                          redirect_uri=redirect_uri)
+
+    Import.import_tracks_from_file(to_token)
+    Import.import_tracks_from_file(to_token)
+    Import.follow_artists_from_file(to_token)
+    os.remove('.cache-' + username_to)
+
+
+# Main Stuff
+
+client_id_from = "e6376f7ecbfb45799240881e368d4d18"
+client_secret_from = "47d3d26c9ba9492a9b0e6cc278712c99"
+username_from = 'c.andersen2012@gmail.com'
+
+client_id_to = "e6376f7ecbfb45799240881e368d4d18"
+client_secret_to = "47d3d26c9ba9492a9b0e6cc278712c99"
+username_to = 'c.andersen2012@gmail.com'
+
+scopes = 'user-library-read user-follow-read playlist-read-private playlist-modify-public playlist-modify-private'
+redirect_uri = "https://google.com"
+
+# UI
+
+top = tk.Tk()
+top.title('Spotify Backup Tool')
+top.geometry("300x250")
+
+client_id_label = tk.Label(top, text='client_id')
+client_id_label.pack()
+
+client_id_entry = tk.Entry(top)
+client_id_entry.pack()
+
+backup_button = tk.Button(top, text='Backup From Spotify', command=backup)
+backup_button.pack()
+
+top.mainloop()
--- spotify_import.py
+++ spotify_import.py
@@ -1,5 +1,37 @@
+import json
+
+import requests
 import spotipy
+from tqdm import tqdm
+
+SPOTIFY_BASE_URL = 'https://api.spotify.com/v1/'
+SPOTIFY_FOLLOW_URL = 'me/following?type=artist&ids='
 
 
-def follow_artists(token):
+def import_tracks_from_file(token):
+    print('Saving tracks to Spotify...')
     sp = spotipy.Spotify(auth=token)
+    tracks_json = json.loads(open('data/tracks.json', 'r').read())
+    sp.current_user_saved_tracks_add(tracks_json)
+    print('Finished saving tracks to spotify')
+
+
+def import_playlists_from_file(token):
+    print("Saving playlists to Spotify...")
+    sp = spotipy.Spotify(auth=token)
+    user_id = sp.me()['id']
+    playlists_json = json.loads(open('data/playlists.json', 'r').read())
+    for playlist in tqdm(playlists_json):
+        create_result = sp.user_playlist_create(user_id, playlist['name'], playlist['public'])
+        playlist_id = create_result['id']
+        sp.user_playlist_add_tracks(user_id, playlist_id, playlist['tracks'])
+
+    print('Finished saving playlists to Spotify.')
+
+
+def follow_artists_from_file(token):
+    print('Saving follows to Spotify...')
+    follows_json = json.loads(open('data/follows.json', 'r').read())
+    headers = {'Authentication': 'Bearer ' + token}
+    requests.put(SPOTIFY_BASE_URL + SPOTIFY_FOLLOW_URL + ','.join(follows_json), headers=headers)
+    print('Finished saving follows to Spotify.')
